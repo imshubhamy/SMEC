599836616ac736fc5abc64133d7b675a7bf2346d
imshubhamy <imshubhamy@gmail.com>
2018-09-02 08:59:53 +0530

    first commit

---
+++ src/java/UploadServlet.java
@@ -0,0 +1,91 @@
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+import java.io.File;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+@WebServlet("/UploadServlet")
+@MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
+                 maxFileSize=1024*1024*10,      // 10MB
+                 maxRequestSize=1024*1024*50)   // 50MB
+public class UploadServlet extends HttpServlet {  
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                      throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+        request.getRequestDispatcher("link.html").include(request, response);  
+          
+        HttpSession session=request.getSession(false);  
+        if(session!=null){  
+        String name=(String)session.getAttribute("name");  
+          
+        out.print("Hello, "+name+" Welcome to Upload"); 
+       // PrintWriter out = response.getWriter();
+    out.println("<h1>File Upload</h1>\n" +
+"    <form method=\"post\" action=\"UploadServlet\"\n" +
+"        enctype=\"multipart/form-data\">\n" +
+"        Select file to upload: <input type=\"file\" name=\"file\" size=\"60\" /><br />\n" +
+"        <br /> <input type=\"submit\" value=\"Upload\" />\n" +
+"    </form>");
+                    request.getRequestDispatcher("message.jsp").include(request, response);  
+
+        }  
+        else{  
+            out.print("Please login first");  
+            request.getRequestDispatcher("login.html").include(request, response);  
+        }  
+        out.close();  
+    }
+    
+    private static final String SAVE_DIR = "uploadFiles";
+     
+    /**
+     * handles file upload
+     */
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        // gets absolute path of the web application
+        String appPath = request.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String savePath = appPath + File.separator + SAVE_DIR;
+         
+        // creates the save directory if it does not exists
+        File fileSaveDir = new File(savePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdir();
+        }
+         
+        for (Part part : request.getParts()) {
+            String fileName = extractFileName(part);
+            // refines the fileName in case it is an absolute path
+            fileName = new File(fileName).getName();
+            part.write(savePath + File.separator + fileName);
+        }
+        request.setAttribute("message", "Upload has been done successfully!");
+        getServletContext().getRequestDispatcher("/message.jsp").forward(
+                request, response);
+    }
+    /**
+     * Extracts file name from HTTP header content-disposition
+     */
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length()-1);
+            }
+        }
+        return "";
+    }
+    
+}  
---
+++ web/admission-send.php
@@ -0,0 +1,36 @@
+
+<?php 
+if(isset($_POST['submit'])){
+    $to = "imshubhamy@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $city = $_POST['city'];
+    $education = $_POST['education'];
+    $course = $_POST['course'];
+    $subject = "Admission form submission";
+    $subject2 = "Copy of your admission form submission of SMEC";
+    $message = " Name :" . $name . " \n Phone no.: " . $phone . "\n Email: " . $email . " \n City: " . $city . " \n Education :" . $education.  " \n Course :" . $course." \n Message:" . "\n \n" . $_POST['message'];
+    $message2 = "Here is a copy of your message \n Name :" . $name . " \n Phone no.: " . $phone . "\n Email:" . $email . " \n City: " . $city ."\n Course :" . $course . " \n Message:" . "\n \n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    
+    
+    
+    }
+?>
+
+    <html>
+    <head></head>
+    <body>
+    <a href="index.html"> Click here to go back to Homepage</a>
+    </body>
+    </html
